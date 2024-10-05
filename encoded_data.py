04d7a33a88c4e69bd9baf3b1e464eb37e1213ab2
VladimirApter <vova.apter@gmail.com>
2024-10-05 11:55:06 +0500

    Add encoded data

---
+++ encoded_data.py
@@ -0,0 +1,5 @@
+class EncodedData:
+    def __init__(self, data_size, intervals_dict, value):
+        self.data_size = data_size
+        self.intervals_dict = intervals_dict
+        self.value = value
